3069ef450593c02a02f2c007b664745a2f2d38dd
Eric Sui <zanzanegi@gmail.com>
2017-05-12 11:26:30 -0400

    Added functionality to edit and update posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,16 @@ def show
   @post = Post.find(params[:id])
 end
 
+def edit
+  @post = Post.find(params[:id])
+end
+
+def update
+  @post = Post.find(params[:id])
+  @post.update(post_params)
+  redirect_to (posts_path(@post))
+end
+
 private
 
 def post_params
