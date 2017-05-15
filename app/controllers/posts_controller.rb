22fcca01dc391a9e68fdc30e8e3be5db5897786b
Eric Sui <zanzanegi@gmail.com>
2017-05-15 17:06:10 -0400

    Added Search bar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+before_action :set_post, only: [:show, :update, :edit, :destroy]
 
 def index
  @posts = Post.all
@@ -8,29 +9,45 @@ def new
   @post = Post.new
 end
 
-def create
-  @post = Post.create(post_params)
-  redirect_to posts_path
-end
-
 def show
-  @post = Post.find(params[:id])
 end
 
 def edit
-  @post = Post.find(params[:id])
+end
+
+def create
+  if @post = Post.create(post_params)
+    flash[:success] = "Your post has been created!"
+    redirect_to posts_path
+  else
+    flash[:alert] = "Post could not be created. Please check the form"
+    render :new
+  end
 end
 
 def update
-  @post = Post.find(params[:id])
-  @post.update(post_params)
-  redirect_to (posts_path(@post))
+  if @post.update(post_params)
+    flash[:success] = "Your post has been updated"
+    redirect_to post_path
+  else
+    flash[:alert] = "Update Failed. Please check form"
+    render :edit
+  end
+end
+
+def destroy
+  @post.destroy
+  redirects_to posts_path
 end
 
 private
 
 def post_params
   params.require(:post).permit(:image, :caption)
+end
+
+def set_post
+  @post = Post.find(params[:id])
 end
 
 end
