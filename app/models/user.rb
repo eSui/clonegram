3f549108ce0e6a6b473c05922ce51a4a8bc481ac
Eric Sui <zanzanegi@gmail.com>
2017-05-18 15:54:30 -0400

    Finished Resgister Page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   validates :user_name, presence: true, length: { minimum: 4, maximum: 16 }
+  validates :password, length: {minimum: 6}
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
